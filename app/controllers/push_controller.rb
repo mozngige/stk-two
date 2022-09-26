1e764a93474bd9a001c98dc2b30657814faeef52
Kihara-Kamotho <kiharaalphan@gmail.com>
2022-09-26 11:58:32 +0300

    push ctrl

---
+++ app/controllers/push_controller.rb
@@ -0,0 +1,10 @@
+class PushController < ApplicationController 
+  # this ctrl initiates payment via the payment action
+  def index 
+    # show all payments 
+  end 
+
+  def payment 
+    # initiate payment by calling payment.new
+  end
+end
