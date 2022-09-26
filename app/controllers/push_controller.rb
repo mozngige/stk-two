d175a2edf5dd4a3003db6e2de1932c887321e23f
Kihara-Kamotho <kiharaalphan@gmail.com>
2022-09-26 18:26:55 +0300

    payment&callback action

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -7,5 +7,66 @@ class PushController < ApplicationController
 
   def payment 
     # initiate payment by calling payment.new
+    # check params 
+    if params.present? 
+      phone = PhonyRails.normalize_number(params[:phone_number], country_code: 'KE').gsub(/\W/, '')
+      # new payment 
+      payment = Payment.new(amount: params[:amount], phone_number: phone)
+
+      if payment.save 
+        redirect_to root_path, :flash => { success: "payment successful"}
+      else 
+        redirect_to root_path, :flash => { alert: "unknown error occurred"} 
+      end
+
+    end
+
+    # rescue exception 
+  rescue Exception => e 
+    p e.message 
   end
+  
+  # callback fnc 
+  def callback 
+    merchantRequestId = params[:Body][:stkCallback][:MerchantRequestID]
+    checkoutRequestId = params[:Body][:stkCallback][:CheckoutRequestID]
+
+    amount, phonenumber, mpesareceiptnumber, transactiondate = nil 
+    # setting the above values will validate a transaction 
+
+    # scope callbackmetadata, to retrieve above data 
+    if params[:Body][:stkCallback][:CallbackMetadata].present? 
+      # iterate thru each item 
+      params[:Body][:stkCallback][:CallbackMetadata][:Item].each do |item| 
+        # for each item 
+        # switch case 
+        case item[:Name].downcase 
+        when 'amount' 
+          amount = item[:Value]
+        when 'mpesareceiptnumber'
+          mpesareceiptnumber = item[:Value] 
+        when 'transactiondate'
+          transactiondate = item[:Value] 
+        when 'phonenumber' 
+          phonenumber = item[:Value]
+        end
+      end
+
+      # retrieve a payment, if payment has been successfully made 
+      pay = Payment.find_by(amount: amount, phonenumber: phonenumber, merchantRequestId: MerchantRequestID, checkoutRequestId: CheckoutRequestID ) # ensure the check matches all these params 
+      # alter state of payment 
+      pay.state = true 
+      pay.code = mpesareceiptnumber
+      pay.save
+
+    else 
+      # if payment was unsuccessful 
+      pay = Payment.find_by(checkoutRequestId: CheckoutRequestID, merchantRequestId: MerchantRequestID) 
+      result_desc = params[:Body][:stkCallback][:ResultDesc]
+      pay.code = result_desc
+      pay.save
+    end
+    # save this transaction 
+    Transaction.create({ callback: params })
+  end 
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,28 @@
 class Payment < ApplicationRecord 
-  
+  # this class processes payments
+  after_create :push_stk 
+  # after_create is called after saving new object to db 
+  # after_create is wrapped in around save  
+
+  def push_stk 
+    # this method invokes code written in push_payment.rb as appropriate 
+    # after a payment is created, call this method on it 
+    begin 
+      response = MpesaStk::PushPayment.call(amount, phone_number)
+      p response 
+      # set these value for a specific transation
+      self.MerchantRequestID = response[:MerchantRequestID]
+      self.CheckoutRequestID = response[:CheckoutRequestID]
+
+      self.response = response 
+      
+      self.save 
+
+    rescue => ex 
+      p ex.message 
+
+      self.response = ex.message 
+      self.save 
+    end
+  end
 end
